f8bd8ff119fecbafb3a0b338fe555ac6c6a50632
Diprish <diprish@gmail.com>
2016-09-19 20:28:59 +0530

    starting scrum2

---
+++ GetCustomersRTS/Resources/BPEL_DBREFERENCE_drop.sql
@@ -0,0 +1,8 @@
+-- uncomment to overwrite existing types
+-- DROP TYPE XXMAF_CUST_CARR_DX890762X1X3 FORCE; 
+-- show errors
+-- uncomment to overwrite existing types
+-- DROP TYPE XXMAF_CUST_CARR_DX890762X1X2 FORCE; 
+-- show errors
+DROP PACKAGE BPEL_DBREFERENCE;
+exit
